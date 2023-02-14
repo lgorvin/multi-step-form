eee17cab3405b02b5d82f97fa733bc8b5bd9984f
lgorvin <lgorvin78@gmail.com>
2023-02-14 16:26:41 +0000

    Addeds props

--- payment-form/src/components/StepOne.tsx
+++ payment-form/src/components/StepOne.tsx
@@ -1,10 +1,31 @@
-import React from "react";
-import { Input } from "@mantine/core";
+import React, { useState } from "react";
+import { Input, Autocomplete } from "@mantine/core";
+import { Dispatch, SetStateAction } from "react";
 
-const StepOne = () => {
+type Props = {
+  name: string;
+  setName: Dispatch<SetStateAction<string>>;
+  email: string;
+  setEmail: Dispatch<SetStateAction<string>>;
+  phone: string;
+  setPhone: Dispatch<SetStateAction<string>>;
+};
+
+const StepOne: React.FC<Props> = (props) => {
+  //   const [name, setName] = useState("" as string);
+  //   const [email, setEmail] = useState("" as string);
+  //   const [phone, setPhone] = useState("" as string);
+  const data =
+    props.email.trim().length > 0 && !props.email.includes("@")
+      ? ["gmail.com", "outlook.com", "yahoo.com", "hotmail.com"].map(
+          (provider) => `${props.email}@${provider}`
+        )
+      : [];
+
   return (
     <>
       <h1 className="mx-6 mt-6 text-blue-900 font-bold">Personal info</h1>
+      <h1>{props.phone}</h1>
       <p className="mx-6 text-gray-400">
         Please provide your name, email address, and phone number.
       </p>
@@ -14,7 +35,12 @@ const StepOne = () => {
         withAsterisk
         label="Name"
       >
-        <Input id="input-demo" placeholder="e.g. Stephen King" />
+        <Input
+          required
+          onChange={(event) => props.setName(event.target.value)}
+          id="input-demo"
+          placeholder="e.g. Stephen King"
+        />
       </Input.Wrapper>
       <Input.Wrapper
         className="mx-6 mt-6"
@@ -22,14 +48,24 @@ const StepOne = () => {
         withAsterisk
         label="Email Address"
       >
-        <Input id="input-demo" placeholder="e.g. stephenking@lorem.com" />
+        {/* <Input id="input-demo" placeholder="e.g. stephenking@lorem.com" /> */}
+        <Autocomplete
+          value={props.email}
+          onChange={props.setEmail}
+          placeholder="e.g. stephenking@lorem.com"
+          data={data}
+        />
       </Input.Wrapper>
       <Input.Wrapper
         className="mx-6 mt-6 mb-8"
         id="input-demo"
         label="Phone Number"
       >
-        <Input id="input-demo" placeholder="e.g. +1 234 567 890" />
+        <Input
+          onChange={(event) => props.setPhone(event.target.value)}
+          id="input-demo"
+          placeholder="e.g. +1 234 567 890"
+        />
       </Input.Wrapper>
     </>
   );
--- payment-form/src/components/StepTwo.tsx
+++ payment-form/src/components/StepTwo.tsx
@@ -5,24 +5,39 @@ import { Switch, Text, Image } from "@mantine/core";
 import arcade from "../assets/images/icon-arcade.svg";
 import advanced from "../assets/images/icon-advanced.svg";
 import pro from "../assets/images/icon-pro.svg";
+import { Dispatch, SetStateAction } from "react";
 
-const StepTwo = () => {
-  const [arcadeBtn, setArcadeBtn] = useState(false as boolean);
+type Props = {
+  arcadeBtn: boolean;
+  setArcadeBtn: Dispatch<SetStateAction<boolean>>;
+  advancedBtn: boolean;
+  setAdvancedBtn: Dispatch<SetStateAction<boolean>>;
+  proBtn: boolean;
+  setProBtn: Dispatch<SetStateAction<boolean>>;
+  monthOrYear: boolean;
+  setMonthOrYear: Dispatch<SetStateAction<boolean>>;
+};
+
+const StepTwo: React.FC<Props> = (props) => {
+  //const [arcadeBtn, setArcadeBtn] = useState(false as boolean);
   const handleArcade = () => {
-    if (!advancedBtn && !proBtn) setArcadeBtn(!arcadeBtn);
-    if (advancedBtn || proBtn) alert("Uncheck your option first!");
+    if (!props.advancedBtn && !props.proBtn)
+      props.setArcadeBtn(!props.arcadeBtn);
+    if (props.advancedBtn || props.proBtn) alert("Uncheck your option first!");
   };
-  const [advancedBtn, setAdvancedBtn] = useState(false as boolean);
+  //const [advancedBtn, setAdvancedBtn] = useState(false as boolean);
   const handleAdvanced = () => {
-    if (!arcadeBtn && !proBtn) setAdvancedBtn(!advancedBtn);
-    if (arcadeBtn || proBtn) alert("Uncheck your option first!");
+    if (!props.arcadeBtn && !props.proBtn)
+      props.setAdvancedBtn(!props.advancedBtn);
+    if (props.arcadeBtn || props.proBtn) alert("Uncheck your option first!");
   };
-  const [proBtn, setProBtn] = useState(false as boolean);
+  //const [proBtn, setProBtn] = useState(false as boolean);
   const handlePro = () => {
-    if (!arcadeBtn && !advancedBtn) setProBtn(!proBtn);
-    if (advancedBtn || arcadeBtn) alert("Uncheck your option first!");
+    if (!props.arcadeBtn && !props.advancedBtn) props.setProBtn(!props.proBtn);
+    if (props.advancedBtn || props.arcadeBtn)
+      alert("Uncheck your option first!");
   };
-  const [monthOrYear, setMonthOrYear] = useState(false as boolean);
+  //const [monthOrYear, setMonthOrYear] = useState(false as boolean);
 
   return (
     <>
@@ -36,7 +51,7 @@ const StepTwo = () => {
         <button
           onClick={handleArcade}
           className={
-            arcadeBtn
+            props.arcadeBtn
               ? "flex items-center w-[86%] mx-6 py-6 rounded-md border-2 group hover:scale-105 duration-300 scale-105 bg-gray-50 border-purple-900"
               : "flex items-center w-[86%] mx-6 py-6 rounded-md border-2 group hover:scale-105 duration-300"
           }
@@ -51,7 +66,7 @@ const StepTwo = () => {
             >
               Arcade
             </Text>
-            {!monthOrYear && (
+            {!props.monthOrYear && (
               <Text
                 className="mt-2  duration-300"
                 color="dimmed"
@@ -63,7 +78,7 @@ const StepTwo = () => {
                 $9/mo
               </Text>
             )}
-            {monthOrYear && (
+            {props.monthOrYear && (
               <>
                 <Text
                   className="mt-2 duration-300"
@@ -92,7 +107,7 @@ const StepTwo = () => {
         <button
           onClick={handleAdvanced}
           className={
-            advancedBtn
+            props.advancedBtn
               ? "flex items-center w-[86%] mx-6 py-6 mt-3 rounded-md border-2 group hover:scale-105 duration-300 scale-105 bg-gray-50 border-purple-900"
               : "flex items-center w-[86%] mx-6 py-6 mt-3 rounded-md border-2 group hover:scale-105 duration-300"
           }
@@ -102,7 +117,7 @@ const StepTwo = () => {
             <Text className="" weight={500} size="md" sx={{ lineHeight: 1 }}>
               Advanced
             </Text>
-            {!monthOrYear && (
+            {!props.monthOrYear && (
               <Text
                 className="mt-2  duration-300"
                 color="dimmed"
@@ -114,7 +129,7 @@ const StepTwo = () => {
                 $12/mo
               </Text>
             )}
-            {monthOrYear && (
+            {props.monthOrYear && (
               <>
                 <Text
                   className="mt-2 duration-300"
@@ -143,7 +158,7 @@ const StepTwo = () => {
         <button
           onClick={handlePro}
           className={
-            proBtn
+            props.proBtn
               ? "flex items-center w-[86%] mx-6 py-6 mt-3 rounded-md border-2 group hover:scale-105 duration-300 scale-105 bg-gray-50 border-purple-900"
               : "flex items-center w-[86%] mx-6 py-6 mt-3 rounded-md border-2 group hover:scale-105 duration-300"
           }
@@ -158,7 +173,7 @@ const StepTwo = () => {
             >
               Pro
             </Text>
-            {!monthOrYear && (
+            {!props.monthOrYear && (
               <Text
                 className="mt-2 duration-300"
                 color="dimmed"
@@ -170,7 +185,7 @@ const StepTwo = () => {
                 $15/mo
               </Text>
             )}
-            {monthOrYear && (
+            {props.monthOrYear && (
               <>
                 <Text
                   className="mt-2 duration-300"
@@ -202,8 +217,10 @@ const StepTwo = () => {
         <div className="bg-gray-100 rounded-md  w-full h-11 flex justify-center items-center">
           <h1 className="text-base mx-8 font-bold">Monthly</h1>
           <Switch
-            checked={monthOrYear}
-            onChange={(event) => setMonthOrYear(event.currentTarget.checked)}
+            checked={props.monthOrYear}
+            onChange={(event) =>
+              props.setMonthOrYear(event.currentTarget.checked)
+            }
             size="md"
             color="blue.7"
           />
