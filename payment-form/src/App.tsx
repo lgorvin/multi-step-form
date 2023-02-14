9e3df62708a23657a4b72edc45197121d433bdd6
lgorvin <lgorvin78@gmail.com>
2023-02-14 12:20:08 +0000

    Logic for changing steps

--- payment-form/src/App.tsx
+++ payment-form/src/App.tsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 import { Button } from "@mantine/core";
 import { MantineProvider, Input } from "@mantine/core";
 import StepOne from "./components/StepOne";
+import StepTwo from "./components/StepTwo";
+import StepThree from "./components/StepThree";
+import StepFour from "./components/StepFour";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [stepOne, setStepOne] = useState(useState(true as boolean));
+  const [stepOne, setStepOne] = useState(true as boolean);
+  const [stepTwo, setStepTwo] = useState(false);
+  const [stepThree, setStepThree] = useState(false as boolean);
+  const [stepFour, setStepFour] = useState(false as boolean);
 
   return (
     <>
@@ -59,22 +65,105 @@ function App() {
           </div>
           <div className=" absolute top-7 left-1/2 transform -translate-x-1/2">
             <div className="grid grid-cols-4 gap-6 items-center">
-              <div className="bg-cyan-200 rounded-full h-9 w-9 flex justify-center items-center">
-                <h1 className="font-bold text-base text-black">1</h1>
+              <div
+                onClick={() => {
+                  setStepOne(true);
+                  setStepTwo(false);
+                  setStepThree(false);
+                  setStepFour(false);
+                }}
+                className={
+                  stepOne
+                    ? "bg-cyan-200 rounded-full h-9 w-9 flex justify-center items-center cursor-pointer transition-colors duration-500 ease-linear"
+                    : "bg-transparent border-2 rounded-full h-9 w-9 flex justify-center items-center cursor-pointer transition-colors duration-500 ease-linear"
+                }
+              >
+                <h1
+                  className={
+                    stepOne
+                      ? "font-bold text-base text-black duration-300"
+                      : "font-bold text-base text-white duration-300"
+                  }
+                >
+                  1
+                </h1>
               </div>
-              <div className="bg-transparent border-2 rounded-full h-9 w-9 flex justify-center items-center">
-                <h1 className="font-bold text-base text-white">2</h1>
+              <div
+                onClick={() => {
+                  setStepOne(false);
+                  setStepTwo(true);
+                  setStepThree(false);
+                  setStepFour(false);
+                }}
+                className={
+                  stepTwo
+                    ? "bg-cyan-200 rounded-full h-9 w-9 flex justify-center items-center cursor-pointer transition-colors duration-500 ease-linear"
+                    : "bg-transparent border-2 rounded-full h-9 w-9 flex justify-center items-center cursor-pointer transition-colors duration-500 ease-linear"
+                }
+              >
+                <h1
+                  className={
+                    stepTwo
+                      ? "font-bold text-base text-black duration-300"
+                      : "font-bold text-base text-white duration-300"
+                  }
+                >
+                  2
+                </h1>
               </div>
-              <div className="bg-transparent border-2 rounded-full h-9 w-9 flex justify-center items-center">
-                <h1 className="font-bold text-base text-white">3</h1>
+              <div
+                onClick={() => {
+                  setStepOne(false);
+                  setStepTwo(false);
+                  setStepThree(true);
+                  setStepFour(false);
+                }}
+                className={
+                  stepThree
+                    ? "bg-cyan-200 rounded-full h-9 w-9 flex justify-center items-center cursor-pointer transition-colors duration-500"
+                    : "bg-transparent border-2 rounded-full h-9 w-9 flex justify-center items-center cursor-pointer transition-colors duration-500"
+                }
+              >
+                <h1
+                  className={
+                    stepThree
+                      ? "font-bold text-base text-black duration-300"
+                      : "font-bold text-base text-white duration-300"
+                  }
+                >
+                  3
+                </h1>
               </div>
-              <div className="bg-transparent border-2 rounded-full h-9 w-9 flex justify-center items-center">
-                <h1 className="font-bold text-base text-white">4</h1>
+              <div
+                onClick={() => {
+                  setStepOne(false);
+                  setStepTwo(false);
+                  setStepThree(false);
+                  setStepFour(true);
+                }}
+                className={
+                  stepFour
+                    ? "bg-cyan-200 rounded-full h-9 w-9 flex justify-center items-center cursor-pointer transition-colors duration-500"
+                    : "bg-transparent border-2 rounded-full h-9 w-9 flex justify-center items-center cursor-pointer transition-colors duration-500"
+                }
+              >
+                <h1
+                  className={
+                    stepFour
+                      ? "font-bold text-base text-black duration-300"
+                      : "font-bold text-base text-white duration-300"
+                  }
+                >
+                  4
+                </h1>
               </div>
             </div>
           </div>
           <div className="bg-white absolute top-[99px] left-1/2 transform -translate-x-1/2 h-[425px] w-[350px] rounded-lg shadow-lg">
             {stepOne && <StepOne />}
+            {stepTwo && <StepTwo />}
+            {stepThree && <StepThree />}
+            {stepFour && <StepFour />}
           </div>
 
           <div
@@ -82,12 +171,46 @@ function App() {
               boxShadow:
                 "0 -4px 16px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
             }}
-            className="bg-white w-screen h-24  absolute bottom-0 left-1/2 transform -translate-x-1/2"
+            className="bg-white w-screen h-24 absolute bottom-0 left-1/2 transform -translate-x-1/2"
           >
             <div className="flex items-center h-full w-full">
+              {!stepOne && (
+                <button
+                  onClick={() => {
+                    if (stepTwo) {
+                      setStepTwo(false);
+                      setStepOne(true);
+                    } else if (stepThree) {
+                      setStepThree(false);
+                      setStepTwo(true);
+                    } else if (stepFour) {
+                      setStepFour(false);
+                      setStepThree(true);
+                    }
+                  }}
+                  className="px-4 py-3 ml-2 active:scale-95 duration-300 rounded-md text-gray-400"
+                >
+                  Go Back
+                </button>
+              )}
               <button
-                onClick={() => {}}
-                className="bg-blue-900 px-4 py-3 ml-[256px] mr-4 active:scale-95 duration-300 rounded-md text-white"
+                onClick={() => {
+                  if (stepOne) {
+                    setStepOne(false);
+                    setStepTwo(true);
+                  } else if (stepTwo) {
+                    setStepTwo(false);
+                    setStepThree(true);
+                  } else if (stepThree) {
+                    setStepThree(false);
+                    setStepFour(true);
+                  }
+                }}
+                className={
+                  !stepOne
+                    ? "bg-blue-900 px-4 py-3 ml-[155px] active:scale-95 duration-300 rounded-md text-white"
+                    : "bg-blue-900 px-4 py-3 ml-[255px] active:scale-95 duration-300 rounded-md text-white"
+                }
               >
                 Next Step
               </button>
