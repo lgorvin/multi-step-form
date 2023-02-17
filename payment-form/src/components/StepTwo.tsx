a5f13f7f21a18c1348a1f738157ab4e33fa3d994
lgorvin <lgorvin78@gmail.com>
2023-02-17 14:22:29 +0000

    Responsive styling - Step two page

--- payment-form/src/components/StepTwo.tsx
+++ payment-form/src/components/StepTwo.tsx
@@ -6,6 +6,7 @@ import arcade from "../assets/images/icon-arcade.svg";
 import advanced from "../assets/images/icon-advanced.svg";
 import pro from "../assets/images/icon-pro.svg";
 import { Dispatch, SetStateAction } from "react";
+import { useMediaQuery } from "react-responsive";
 
 type Props = {
   arcadeBtn: boolean;
@@ -21,6 +22,7 @@ type Props = {
 };
 
 const StepTwo: React.FC<Props> = (props) => {
+  const tailwindMd = useMediaQuery({ query: "(min-width: 768px)" });
   const handleArcade = () => {
     if (!props.advancedBtn && !props.proBtn)
       props.setArcadeBtn(!props.arcadeBtn);
@@ -65,26 +67,45 @@ const StepTwo: React.FC<Props> = (props) => {
 
   return (
     <>
-      <h1 className="mx-6 mt-6 text-blue-900 font-bold">Select your plan</h1>
-      <p className="mx-6 mb-4 text-gray-400">
+      <h1 className="mx-6 mt-6 md:ml-[340px] md:mr-20 duration-500 text-blue-900 font-bold">
+        Select your plan
+      </h1>
+      <p className="mx-6 mb-4 md:ml-[340px] md:mr-20 duration-500 text-gray-400">
         You have the option for monthly or yearly billing.
       </p>
 
-      <div>
+      <div className="md:grid md:grid-cols-3 md:w-[50%] md:ml-[340px] duration-500">
         <button
           onClick={() => {
             handleArcade();
           }}
           className={
             props.arcadeBtn
-              ? "flex items-center w-[86%] mx-6 py-6 rounded-md border-2 group hover:scale-105 duration-300 scale-105 bg-gray-50 border-purple-900"
-              : "flex items-center w-[86%] mx-6 py-6 rounded-md border-2 group hover:scale-105 duration-300"
+              ? "flex items-center w-[86%] md:w-[105%] md:h-[200px] md:mx-0 mx-6 py-6 rounded-md border-2 group hover:scale-105 duration-300 scale-105 bg-gray-50 border-purple-900"
+              : "flex items-center w-[86%] md:w-[105%] md:h-[200px] md:mx-0 mx-6 py-6 rounded-md border-2 group hover:scale-105 duration-300"
           }
         >
-          <Image className="ml-4" src={arcade} alt="arcade" width={40} />
+          {!tailwindMd && (
+            <Image
+              className="ml-4 md:ml-0"
+              src={arcade}
+              alt="arcade"
+              width={40}
+            />
+          )}
           <div className="ml-4">
+            {tailwindMd && (
+              <Image
+                className="ml-4 mt-0 md:ml-0"
+                src={arcade}
+                alt="arcade"
+                width={50}
+              />
+            )}
             <Text
-              className="mr-11"
+              className={`mr-11 md:mt-16 ${
+                props.monthOrYear ? "md:mt-10" : "md:mt-16"
+              } duration-300`}
               weight={500}
               size="lg"
               sx={{ lineHeight: 1 }}
@@ -133,14 +154,31 @@ const StepTwo: React.FC<Props> = (props) => {
           onClick={handleAdvanced}
           className={
             props.advancedBtn
-              ? "flex items-center w-[86%] mx-6 py-6 mt-3 rounded-md border-2 group hover:scale-105 duration-300 scale-105 bg-gray-50 border-purple-900"
-              : "flex items-center w-[86%] mx-6 py-6 mt-3 rounded-md border-2 group hover:scale-105 duration-300"
+              ? "flex items-center w-[86%] md:w-[105%] md:h-[200px] md:mx-4 mx-6 py-6 mt-3 md:mt-0 rounded-md border-2 group hover:scale-105 duration-300 scale-105 bg-gray-50 border-purple-900"
+              : "flex items-center w-[86%] md:w-[105%] md:h-[200px] md:mx-4 mx-6 py-6 mt-3 md:mt-0 rounded-md border-2 group hover:scale-105 duration-300"
           }
         >
-          <Image className="ml-4" src={advanced} alt="arcade" width={40} />
+          {!tailwindMd && (
+            <Image
+              className="ml-4 md:ml-0 "
+              src={advanced}
+              alt="advanced"
+              width={40}
+            />
+          )}
           <div className="ml-4">
+            {tailwindMd && (
+              <Image
+                className="ml-4 md:ml-0 "
+                src={advanced}
+                alt="advanced"
+                width={50}
+              />
+            )}
             <Text
-              className="mr-11"
+              className={`mr-11 md:mt-16 ${
+                props.monthOrYear ? "md:mt-10" : "md:mt-16"
+              } duration-300`}
               weight={500}
               size="lg"
               sx={{ lineHeight: 1 }}
@@ -189,14 +227,21 @@ const StepTwo: React.FC<Props> = (props) => {
           onClick={handlePro}
           className={
             props.proBtn
-              ? "flex items-center w-[86%] mx-6 py-6 mt-3 rounded-md border-2 group hover:scale-105 duration-300 scale-105 bg-gray-50 border-purple-900"
-              : "flex items-center w-[86%] mx-6 py-6 mt-3 rounded-md border-2 group hover:scale-105 duration-300"
+              ? "flex items-center w-[86%] md:w-[105%] md:h-[200px] md:mx-8 mx-6 py-6 mt-3 md:mt-0 rounded-md border-2 group hover:scale-105 duration-300 scale-105 bg-gray-50 border-purple-900"
+              : "flex items-center w-[86%] md:w-[105%] md:h-[200px] md:mx-8 mx-6 py-6 mt-3 md:mt-0 rounded-md border-2 group hover:scale-105 duration-300"
           }
         >
-          <Image className="ml-4" src={pro} alt="arcade" width={40} />
+          {!tailwindMd && (
+            <Image className="ml-4" src={pro} alt="pro" width={40} />
+          )}
           <div className="ml-5">
+            {tailwindMd && (
+              <Image className="" src={pro} alt="pro" width={50} />
+            )}
             <Text
-              className="mr-20"
+              className={`mr-20 md:mt-16 ${
+                props.monthOrYear ? "md:mt-10" : "md:mt-16"
+              } duration-300`}
               weight={500}
               size="lg"
               sx={{ lineHeight: 1 }}
@@ -243,8 +288,8 @@ const StepTwo: React.FC<Props> = (props) => {
         </button>
       </div>
 
-      <div className="flex justify-center mx-6 mt-4 mb-6">
-        <div className="bg-gray-100 rounded-md  w-full h-11 flex justify-center items-center">
+      <div className="flex justify-center mx-6 md:w-[465px] md:mt-6 md:mx-6 md:mb-[-10px] md:ml-[340px] mt-4 mb-6 duration-300">
+        <div className="bg-gray-100 rounded-md  w-full h-11 md:h-14 flex justify-center items-center duration-300">
           <h1 className="text-base mx-8 font-bold">Monthly</h1>
           <Switch
             checked={props.monthOrYear}
